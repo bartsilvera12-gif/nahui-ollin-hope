76068091ae3644fc62cd23302e8f877f73b99364
Bart Silvera <bartsilvera12@gmail.com>
2026-05-19 13:49:25 -0300

    Cambiar icono de Evangelizacion a cruz

--- src/components/site/About.tsx
+++ src/components/site/About.tsx
@@ -11,6 +11,7 @@ import {
   GraduationCap,
   Home,
   Megaphone,
+  Cross,
 } from "lucide-react";
 import { aboutContent, values } from "@/data/site";
 import crFeedinKids from "@/assets/cr-feedin-kids.jpg";
@@ -25,6 +26,7 @@ const iconMap = {
   GraduationCap,
   Home,
   Megaphone,
+  Cross,
 } as const;
 
 type IconKey = keyof typeof iconMap;
